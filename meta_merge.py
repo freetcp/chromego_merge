11d808f14210e3d9d17aa8cd5f22097cd2d5ce3e
Richard Pritchett <28760346+freetcp@users.noreply.github.com>
2025-06-01 07:33:01 +0800

    Update meta_merge.py

--- meta_merge.py
+++ meta_merge.py
@@ -1,5 +1,3 @@
-import time
-
 import yaml
 import json
 import urllib.request
@@ -7,73 +5,64 @@ import logging
 import geoip2.database
 import socket
 import re
-import requests
-from get_location import get_physical_location
+
 
 # 提取节点
 def process_urls(url_file, processor):
     try:
-        with open(url_file, 'r') as file:
+        with open(url_file, "r") as file:
             urls = file.read().splitlines()
 
         for index, url in enumerate(urls):
             try:
                 response = urllib.request.urlopen(url)
-                data = response.read().decode('utf-8')
+                data = response.read().decode("utf-8")
                 processor(data, index)
             except Exception as e:
                 logging.error(f"Error processing URL {url}: {e}")
     except Exception as e:
         logging.error(f"Error reading file {url_file}: {e}")
-#提取clash节点
+
+
+# 提取clash节点
 def process_clash(data, index):
     content = yaml.safe_load(data)
-    proxies = content.get('proxies', [])
+    proxies = content.get("proxies", [])
     for i, proxy in enumerate(proxies):
-        location = get_physical_location(proxy['server'])
-        #print(location)
-
-        proxy['name'] = f"{location}_{proxy['type']}_{index}{i+1}"
-        #print(proxy)
-
+        location = get_physical_location(proxy["server"])
+        proxy["name"] = f"{location}_{proxy['type']}_{index}{i+1}"
     merged_proxies.extend(proxies)
 
-'''
+
 def get_physical_location(address):
-    address = re.sub(':.*', '', address)  # 用正则表达式去除端口部分
+    address = re.sub(":.*", "", address)  # 用正则表达式去除端口部分
     try:
         ip_address = socket.gethostbyname(address)
     except socket.gaierror:
         ip_address = address
-    #url = 'https://api.ip2location.io/?key=3B23CB365F21EE4FA890AC0C2A9FC046&ip='+ip_address
 
     try:
-        #response = requests.get(url)
-        #time.sleep(1)
-        reader = geoip2.database.Reader('GeoLite2-City.mmdb')  # 这里的路径需要指向你自己的数据库文件
+        reader = geoip2.database.Reader(
+            "GeoLite2-City.mmdb"
+        )  # 这里的路径需要指向你自己的数据库文件
         response = reader.city(ip_address)
         country = response.country.name
-        #country = response.json()['country_code']
-        #return f"{country}_{city}"
-        return f"{country}"
+        city = response.city.name
+        return f"{country}_{city}"
+        # return f"油管绵阿羊_{country}"
     except geoip2.errors.AddressNotFoundError as e:
         print(f"Error: {e}")
         return "Unknown"
-'''
 
 
 # 处理sb，待办
 def process_sb(data, index):
-
     try:
         json_data = json.loads(data)
         # 处理 shadowtls 数据
 
         # 提取所需字段
-        method = ''
-        if "method" in json_data["outbounds"][0]:
-            method = json_data["outbounds"][0]["method"]
-
+        method = json_data["outbounds"][0]["method"]
         password = json_data["outbounds"][0]["password"]
         server = json_data["outbounds"][1]["server"]
         server_port = json_data["outbounds"][1]["server_port"]
@@ -95,8 +84,8 @@ def process_sb(data, index):
             "plugin-opts": {
                 "host": server_name,
                 "password": shadowtls_password,
-                "version": int(version)
-            }
+                "version": int(version),
+            },
         }
 
         # 将当前proxy字典添加到所有proxies列表中
@@ -104,6 +93,7 @@ def process_sb(data, index):
 
     except Exception as e:
         logging.error(f"Error processing shadowtls data for index {index}: {e}")
+
 
 def process_hysteria(data, index):
     try:
@@ -121,7 +111,7 @@ def process_hysteria(data, index):
             mport = ports_slt[1]
         else:
             mport = server_port
-        #fast_open = json_data["fast_open"]
+        # fast_open = json_data["fast_open"]
         fast_open = True
         insecure = json_data["insecure"]
         server_name = json_data["server_name"]
@@ -138,13 +128,13 @@ def process_hysteria(data, index):
             "port": server_port,
             "ports": mport,
             "auth_str": auth,
-            "up": 80,
-            "down": 100,
+            "up": 1000,
+            "down": 1000,
             "fast-open": fast_open,
             "protocol": protocol,
             "sni": server_name,
             "skip-cert-verify": insecure,
-            "alpn": [alpn]
+            "alpn": [alpn],
         }
 
         # 将当前proxy字典添加到所有proxies列表中
@@ -152,6 +142,8 @@ def process_hysteria(data, index):
 
     except Exception as e:
         logging.error(f"Error processing hysteria data for index {index}: {e}")
+
+
 # 处理hysteria2
 def process_hysteria2(data, index):
     try:
@@ -165,7 +157,7 @@ def process_hysteria2(data, index):
         ports = server_ports_slt[1]
         ports_slt = ports.split(",")
         server_port = int(ports_slt[0])
-        #fast_open = json_data["fastOpen"]
+        # fast_open = json_data["fastOpen"]
         fast_open = True
         insecure = json_data["tls"]["insecure"]
         sni = json_data["tls"]["sni"]
@@ -181,7 +173,7 @@ def process_hysteria2(data, index):
             "password": auth,
             "fast-open": fast_open,
             "sni": sni,
-            "skip-cert-verify": insecure
+            "skip-cert-verify": insecure,
         }
 
         # 将当前proxy字典添加到所有proxies列表中
@@ -190,16 +182,16 @@ def process_hysteria2(data, index):
     except Exception as e:
         logging.error(f"Error processing hysteria2 data for index {index}: {e}")
 
-#处理xray
+
+# 处理xray
 def process_xray(data, index):
     try:
         json_data = json.loads(data)
-        #print(json_data)
         # 处理 xray 数据
         protocol = json_data["outbounds"][0]["protocol"]
-        #vless操作
+        # vless操作
         if protocol == "vless":
-        # 提取所需字段
+            # 提取所需字段
             server = json_data["outbounds"][0]["settings"]["vnext"][0]["address"]
             port = json_data["outbounds"][0]["settings"]["vnext"][0]["port"]
             uuid = json_data["outbounds"][0]["settings"]["vnext"][0]["users"][0]["id"]
@@ -207,20 +199,23 @@ def process_xray(data, index):
             flow = json_data["outbounds"][0]["settings"]["vnext"][0]["users"][0]["flow"]
             # 传输方式
             network = json_data["outbounds"][0]["streamSettings"]["network"]
-            publicKey = None
-            shortId = None
-            serverName = None
-            fingerprint = None
-            if 'realitySettings' in json_data["outbounds"][0]["streamSettings"]:
-                publicKey = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["publicKey"]
-                shortId = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["shortId"]
-                serverName = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["serverName"]
-                fingerprint = json_data["outbounds"][0]["streamSettings"]["realitySettings"]["fingerprint"]
+            publicKey = json_data["outbounds"][0]["streamSettings"]["realitySettings"][
+                "publicKey"
+            ]
+            shortId = json_data["outbounds"][0]["streamSettings"]["realitySettings"][
+                "shortId"
+            ]
+            serverName = json_data["outbounds"][0]["streamSettings"]["realitySettings"][
+                "serverName"
+            ]
+            fingerprint = json_data["outbounds"][0]["streamSettings"][
+                "realitySettings"
+            ]["fingerprint"]
             # udp转发
             isudp = True
             location = get_physical_location(server)
             name = f"{location}_reality_{index}"
-            
+
             # 根据network判断tcp
             if network == "tcp":
                 proxy = {
@@ -234,16 +229,16 @@ def process_xray(data, index):
                     "udp": isudp,
                     "flow": flow,
                     "client-fingerprint": fingerprint,
-                    "servername": serverName,                
-                    "reality-opts":{
-                        "public-key": publicKey,
-                        "short-id": shortId}
+                    "servername": serverName,
+                    "reality-opts": {"public-key": publicKey, "short-id": shortId},
                 }
-                
+
             # 根据network判断grpc
             elif network == "grpc":
-                serviceName = json_data["outbounds"][0]["streamSettings"]["grpcSettings"]["serviceName"]
-                
+                serviceName = json_data["outbounds"][0]["streamSettings"][
+                    "grpcSettings"
+                ]["serviceName"]
+
                 # 创建当前网址的proxy字典
                 proxy = {
                     "name": name,
@@ -257,12 +252,8 @@ def process_xray(data, index):
                     "flow": flow,
                     "client-fingerprint": fingerprint,
                     "servername": serverName,
-                    "grpc-opts":{
-                        "grpc-service-name": serviceName
-                    },
-                    "reality-opts":{
-                        "public-key": publicKey,
-                        "short-id": shortId}
+                    "grpc-opts": {"grpc-service-name": serviceName},
+                    "reality-opts": {"public-key": publicKey, "short-id": shortId},
                 }
 
         # 将当前proxy字典添加到所有proxies列表中
@@ -270,21 +261,24 @@ def process_xray(data, index):
     except Exception as e:
         logging.error(f"Error processing xray data for index {index}: {e}")
 
+
 def update_proxy_groups(config_data, merged_proxies):
-    for group in config_data['proxy-groups']:
-        if group['name'] in ['自动选择', '节点选择']:
-            if 'proxies' not in group or not group['proxies']:
-                group['proxies'] = [proxy['name'] for proxy in merged_proxies]
+    for group in config_data["proxy-groups"]:
+        if group["name"] in ["自动选择", "节点选择"]:
+            if "proxies" not in group or not group["proxies"]:
+                group["proxies"] = [proxy["name"] for proxy in merged_proxies]
             else:
-                group['proxies'].extend(proxy['name'] for proxy in merged_proxies)
+                group["proxies"].extend(proxy["name"] for proxy in merged_proxies)
+
 
 def update_warp_proxy_groups(config_warp_data, merged_proxies):
-    for group in config_warp_data['proxy-groups']:
-        if group['name'] in ['自动选择', '手动选择', '负载均衡']:
-            if 'proxies' not in group or not group['proxies']:
-                group['proxies'] = [proxy['name'] for proxy in merged_proxies]
+    for group in config_warp_data["proxy-groups"]:
+        if group["name"] in ["自动选择", "手动选择", "负载均衡"]:
+            if "proxies" not in group or not group["proxies"]:
+                group["proxies"] = [proxy["name"] for proxy in merged_proxies]
             else:
-                group['proxies'].extend(proxy['name'] for proxy in merged_proxies)
+                group["proxies"].extend(proxy["name"] for proxy in merged_proxies)
+
 
 # 包含hysteria2
 merged_proxies = []
